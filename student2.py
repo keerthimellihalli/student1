64eac8c4d660460bc417f1ef44fa25bbc8cdd8cf
keerthimellihalli <keerthimellihalli@gmail.com>
2025-11-22 10:56:05 +0530

    student2.py

---
+++ student2.py
@@ -0,0 +1,15 @@
+import sys
+
+if len(sys.argv) == 3:
+  script_name = sys.argv[0]
+  name = sys.argv[2]
+  print(user provided input value:")
+else:
+  script_name = sys.argv[0]
+  name = "Basamma"
+  rollno = "251"
+  print("no input given -using default values:")
+
+print("script name:",script_name)
+print("Student name:",name)
+print("Roll number:",rollno)
